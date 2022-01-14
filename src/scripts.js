9e0a39e3f07b3cac19903b7c6ededafbd87f07e1
Cesare Gallo <cesaregallo0@gmail.com>
2022-01-13 22:38:16 -0700

    Finish booking class

--- src/scripts.js
+++ src/scripts.js
@@ -3,6 +3,9 @@
 
 // An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
+// import moment from 'moment';
+// moment().format();
+// console.log(moment);
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
