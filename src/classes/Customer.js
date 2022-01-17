e97df11a0db1a234f345e15f3654ece978d585c8
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-01-16 21:51:45 -0700

    Update classes

--- src/classes/Customer.js
+++ src/classes/Customer.js
@@ -1,10 +1,10 @@
+import User from './User';
 import moment from 'moment';
 moment().format();
 
-class Customer {
+class Customer extends User {
   constructor(customerData) {
-    this.id = customerData.id;
-    this.name = customerData.name;
+    super(customerData)
     this.allBookings = [];
     this.pastBookings = [];
     this.upcomingBookings = [];
@@ -42,6 +42,27 @@ class Customer {
       return acc;
     }, 0)
     return Number(totalCost.toFixed(2));
+  }
+
+  greetCurrentUser() {
+    let firstName = this.name.split(' ')[0];
+    return `Welcome, ${firstName}!`;
+  }
+    
+  //   gatherCustomerBookings(bookings) {
+  //     this.allBookings = bookings.filter(booking => booking.userID === this.id);
+  //     return this.allBookings.sort((currentBooking, nextBooking) => {
+  //       currentBooking.date < nextBooking.date ?
+  //         -1 : 1;
+  //     });
+  //   } 
+      
+  sortBookings(bookingData, currentDate) {
+    let myBookings = this.gatherCustomerBookings(bookingData);
+    console.log(myBookings)
+    myBookings.forEach(booking => {
+      this.organizeBookings(booking, currentDate);
+    })
   }
 
 }
