67618646f546708f50a7fc9e1c04be57576634f8
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-01-17 18:49:38 -0700

    Add render available rooms function for dashboard

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -9,62 +9,114 @@ const reservationInfo = document.getElementById("reservationInfo")
 const upcomingReservations = document.getElementById("upcoming");
 const pastReservations = document.getElementById("past")
 
+const availableRoomsSection = document.getElementById("availableRooms")
+const searchDate = document.getElementById("bookingDate");
+const searchRoomType = document.getElementById("roomType");
+
 function populateBookings(bookings, roomsData, reservationSection) {
-    console.log(bookings)
-    bookings.forEach(booking => {
-        const bookedRoom = roomsData.find(room => {
-            return booking.roomNumber === room.number;
-        });
+  bookings.forEach(booking => {
+    const bookedRoom = roomsData.find(room => {
+      return booking.roomNumber === room.number;
+    });
 
-        const bookingDate = moment(booking.date);
-        const formattedDate = bookingDate.format('MMM D YYYY');
+    const bookingDate = moment(new Date(booking.date));
+    const formattedDate = bookingDate.format('MMM D YYYY');
 
-        reservationSection.innerHTML += `
+    reservationSection.innerHTML += `
         <div>
-        <article class="card">
+        <article class="booking-card">
             <div class="past-date">
                 <h2 class="date">${formattedDate}</h2>
                 <h3 class="room-type">${bookedRoom.roomType}</h3>
             </div>
-            <div class="text-area">
+            <div class="booking-info">
                 <p id="typeOfBed">${bookedRoom.numBeds} ${bookedRoom.bedSize}</p>
                 <p id="costPerNight">$${bookedRoom.costPerNight} per night</p>
             </div>
         </article>
         </div>
         `
-    });
+  });
 }
 
 let domUpdates = {
-    renderDashboard(customer, roomsData) {
-        welcomeMsg.innerText = customer.greetCurrentUser();
-        totalSpent.innerText = customer.calculateTotalSpent(roomsData);
+  renderDashboard(customer, roomsData) {
+    welcomeMsg.innerText = customer.greetCurrentUser();
+    totalSpent.innerText = customer.calculateTotalSpent(roomsData);
 
-        if (customer.allBookings.length === 0) {
-            reservationInfo.innerHTML += "<p>Sorry, you have no past or upcoming reservations. Book now!</p>"
-        }
-
-        if (customer.upcomingBookings.length > 0) {
-            populateBookings(customer.upcomingBookings, roomsData, upcomingReservations)
-        } else {
-            upcomingReservations.innerHTML += "<p>Sorry, you have no upcoming reservations. Book one now!</p>"
-        }
-
-        if (customer.pastBookings.length > 0) {
-            populateBookings(customer.pastBookings, roomsData, pastReservations)
-        } else {
-            pastReservations.innerHTML += "<p>Sorry, you have no upcoming reservations. Book one now!</p>"
-        }
-
-        // room reservations
-  
-    },
-    showModal(modal) {
-        modal.style.display = "block";
-    },
-    closeModal(modal) {
-        modal.style.display = "none";
+    if (customer.allBookings.length === 0) {
+      reservationInfo.innerHTML += `<p class="reservation-msg">Sorry, you have no past or upcoming reservations. Book now!</p>`;
     }
+
+    if (customer.upcomingBookings.length > 0) {
+      populateBookings(customer.upcomingBookings, roomsData, upcomingReservations)
+    } else {
+      upcomingReservations.innerHTML += `<p class="reservation-msg">Sorry, you have no upcoming reservations. Book one now!</p>`;
+    }
+
+    if (customer.pastBookings.length > 0) {
+      populateBookings(customer.pastBookings, roomsData, pastReservations)
+    } else {
+      pastReservations.innerHTML += `<p class="reservation-msg">Sorry, you have no upcoming reservations. Book one now!</p>`;
+    }
+
+    availableRoomsSection.innerHTML = `<p class="reservation-msg">Select a room by availability date and/or type of room.</p>`;
+  },
+  renderAvailableRooms(searchCriteria, currentUser) {
+		
+    availableRoomsSection.innerHTML = '';
+
+    if (currentUser.availableRooms.length < 1) {
+      availableRoomsSection.innerHTML = `
+			<p>Sorry ${currentUser.name}, 
+			no rooms were found for date: ${searchCriteria.date}
+			for selected type: ${searchCriteria.type} 
+			Please adjust selection and try again.
+			</p>
+			`;
+    }
+
+    currentUser.availableRooms.forEach(room => {
+      availableRoomsSection.innerHTML += `
+            <div class="room-card" id="${room.number}">
+            <div class="img-container">
+                <img 
+								src="${room.imageURL}"
+								class="room-thumbnail" 
+								alt="${room.alt}"
+								>
+            </div>
+              <div class="room-info">
+                <h4 class="room-type">Room Type: ${room.roomType}</h4>
+								<h4 class="room-cost">Price Per Night: $${room.costPerNight}</h4>
+								<ul class="room-bed-info" style="list-style-type:none">
+									<li>Beds: ${room.numBeds} </li>
+									<li>Size: ${room.bedSize} </li>
+									<li>Bidet: ${room.bidet ? "Yes" : "No"}</li>
+								</ul>
+              </div>
+							<button class="bookRoomButton" class="book-room-button">Book Room</button>
+            </div>
+          `;
+    });
+  },
+  getSearchCriteria() {
+    const date = searchDate.value;
+    const type = searchRoomType.value;
+
+    return {date, type}
+  },
+  clearSearchCriteria() {
+    searchDate.value = '';
+    searchRoomType.selectedIndex = "0";
+  },
+  showModal(modal) {
+    modal.classList.add("showModal")
+    modal.classList.remove("hideModal")
+  },
+  closeModal(modal) {
+    modal.classList.add("hideModal")
+    modal.classList.remove("showModal")
+  }
 }
 export default domUpdates;
