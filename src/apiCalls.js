87af9cd77c95c37c50c4fe2567c3786e3c0b2597
Cesare Gallo <78453792+cagallo@users.noreply.github.com>
2022-01-18 02:05:42 -0700

    Change get requests to functions

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,11 +1,26 @@
 let roomsApi = fetch("http://localhost:3001/api/v1/rooms")
   .then(response => response.json())
 
-let bookingsApi = fetch("http://localhost:3001/api/v1/bookings")
-  .then(response => response.json())
+let bookingsApi = () => {
+  return fetch("http://localhost:3001/api/v1/bookings")
+    .then(response => response.json())
+}
 
 let usersApi = fetch("http://localhost:3001/api/v1/customers")
   .then(response => response.json())
 
-
-export {roomsApi, bookingsApi, usersApi}
+let postBooking = (bookingData, id) => {
+  const booking = {
+    "userID": id,
+    "date": bookingData.date,
+    "roomNumber": bookingData.roomNumber,
+  }
+  return fetch("http://localhost:3001/api/v1/bookings", {
+    method: 'POST',
+    body: JSON.stringify(booking),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => response.json())
+}
+export {roomsApi, bookingsApi, usersApi, postBooking}
